684968588a97f1b19f2420de8a679d5bf849004d
flumiie <ferickwar@gmail.com>
2023-07-17 17:35:44 +0700

    Add portal scene in plain three format (for references)

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import PortalPlain from "./Portal.Plain";
+
+export { Home, PortalPlain };
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     viteRawPlugin({
-      fileRegex: /\.glsl$/,
+      fileRegex: /\.(glsl|js)$/,
     }),
   ],
 });
