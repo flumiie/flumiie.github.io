93169489b564b962667324c859f099c21ef44fb2
flumiie <ferickwar@gmail.com>
2024-03-15 11:13:11 +0700

    Update 2024.03.15.002: GitHub Next Auth

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # My Portfolio in Next.js
+
+## Development
+
+Set `NEXTAUTH_SECRET = [your_generated_secret]` variable in `.env.local` by using this command:
+```
+opensl rand -base64 32
+```
--- src/componyents/navbar.tsx
+++ src/componyents/navbar.tsx
@@ -100,11 +100,11 @@ const Navbar = () => {
             <div className='sparkle'></div>
           </span>
         </Popover.Group>
-
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a id="nav-login" href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
-          </a> */}
+            <div className='sparkle'></div>
+          </a>
         </div>
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -163,14 +163,14 @@ const Navbar = () => {
                   Contacts
                 </span>
               </div>
-              {/* <div className="py-6">
+              <div className="py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-rose-200"
                 >
                   Log in
                 </a>
-              </div> */}
+              </div>
             </div>
           </div>
         </Dialog.Panel>
